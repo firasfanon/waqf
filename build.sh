3f3c21426742e6ab236e3f9228fd14479cece5a7
Firas <firasfanon@gmail.com>
2025-11-07 12:39:54 +0200

    Update Flutter to 3.27.1 for Dart 3.6 compatibility

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Download and extract Flutter
-curl -L https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.3-stable.tar.xz | tar -xJ -C /tmp
+# Download Flutter 3.27.0 (has newer Dart)
+curl -L https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.27.1-stable.tar.xz | tar -xJ -C /tmp
 
 # Add Flutter to PATH
 export PATH="/tmp/flutter/bin:$PATH"
